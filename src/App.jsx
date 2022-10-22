9661bf325977b4afb88f9c1020a8fdc95db4e3df
luizfernando <luiz-fsilva37@hotmail.com>
2022-10-22 17:47:45 -0300

    CSS modules

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post"
+import './style.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <Post
+        auth="Gunner"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nesciunt. Consequatur nostrum ad quod error odio quia, quibusdam voluptatem blanditiis nihil deleniti sapiente soluta enim vitae. Dolor quisquam ipsam optio."
+      />
+      <Post
+        auth="Logan"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eius tempora possimus non quaerat provident sunt. Voluptatum cumque reiciendis molestias unde ipsum! Eum repellat eaque dolor labore odio ullam nisi."
+      />
+    </div>
+  )
+}
---
+++ src/Post.jsx
@@ -0,0 +1,9 @@
+export function Post(props) {
+    return (
+        <div>
+            <strong>{props.auth}</strong>
+
+            <p>{props.content}</p>
+        </div>
+    )
+}
