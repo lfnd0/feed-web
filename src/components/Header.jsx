7312cd1bd4208b20c6121a378e1aed403be4c990
luizfernando <luiz-fsilva37@hotmail.com>
2022-10-22 18:32:32 -0300

    Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import logo from '../assets/ignite-feed-logo.svg'
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong >Ignite Feed</strong>
+      <img src={logo} alt="Ignite Feed" />
     </header>
   )
 }
