81fc938868e809d3e7fe7e3d003cc98caca6bf4b
luizfernando <luiz-fsilva37@hotmail.com>
2022-10-22 20:57:08 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar";
+
 import './global.css';
+
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        auth="Gunner"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nesciunt. Consequatur nostrum ad quod error odio quia, quibusdam voluptatem blanditiis nihil deleniti sapiente soluta enim vitae. Dolor quisquam ipsam optio."
-      />
-      <Post
-        auth="Logan"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eius tempora possimus non quaerat provident sunt. Voluptatum cumque reiciendis molestias unde ipsum! Eum repellat eaque dolor labore odio ullam nisi."
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+
+        <main>
+          <Post
+            auth="Gunner"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nesciunt. Consequatur nostrum ad quod error odio quia, quibusdam voluptatem blanditiis nihil deleniti sapiente soluta enim vitae. Dolor quisquam ipsam optio."
+          />
+
+          <Post
+            auth="Logan"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eius tempora possimus non quaerat provident sunt. Voluptatum cumque reiciendis molestias unde ipsum! Eum repellat eaque dolor labore odio ullam nisi."
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1603481546164-959efb269a4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <strong>Gunner</strong>
+        <span>Full-stack developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar o seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
