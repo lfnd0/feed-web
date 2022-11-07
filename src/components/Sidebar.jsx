15e6f4e6d17b9d329f77f1a32eed9c57e809d1b1
luizfernando <luiz-fsilva37@hotmail.com>
2022-11-07 20:36:50 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -10,12 +11,10 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/lfnd0.png"
-        />
+        <Avatar src="https://github.com/lfnd0.png" />
 
         <strong>Luiz Fernando</strong>
+
         <span>Full-stack developer</span>
       </div>
 
