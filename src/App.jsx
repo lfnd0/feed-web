4add92bf83c27fa97b4c66b5cead6a517aceee06
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-02-13 21:16:46 -0300

    Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post"
 import { Sidebar } from "./components/Sidebar";
-
 import './global.css';
 import styles from './App.module.css';
 
@@ -18,7 +17,7 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NWL Return, evento da Rocketseat. O nome do projeto é DoctorCare.' },
       { type: 'link', content: 'lfnd0/doctorcare' },
     ],
-    publishedAt: new Date('2024-02-13 20:11:34')
+    publishedAt: new Date('2024-02-13 20:11:34'),
   },
   {
     id: 2,
@@ -30,9 +29,9 @@ const posts = [
     contents: [
       { type: 'paragraph', content: 'Fala galera!' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NWL Return, evento da Rocketseat. O nome do projeto é DoctorCare.' },
-      { type: 'link', content: 'lfnd0/doctorcare' },
+      { type: 'link', content: 'luancgs/doctorcare' },
     ],
-    publishedAt: new Date('2024-01-17 13:22:42')
+    publishedAt: new Date('2024-01-17 13:22:42'),
   },
 ];
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,38 +1,49 @@
+import { format, formatDistanceToNow } from 'date-fns'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+import ptBR from 'date-fns/locale/pt-BR'
 import styles from './Post.module.css'
 
-export function Post(prosp) {
-  console.log('#props: ', prosp);
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/lfnd0.png" />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Luiz Fernando</strong>
-            <span>Full-Stack Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
         <time
-          title='12 de fevereiro de 2024 às 21:19h'
-          dateTime='2022-02-12 21:19:32'
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
         >
-          Publicado há 1h
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galera!</p>
-        <p>Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NWL Return, evento da Rocketseat. O nome do projeto é DoctorCare.</p>
-        <p><a href="">lfnd0/doctorcare</a></p>
-        <p>
-          <a href="">#nlw</a>{' '}
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#rockeseat</a>{' '}
-        </p>
+        {
+          content.map(line => {
+            if(line.type === 'paragraph') {
+              return <p>{line.content}</p>
+            } else {
+              return <p><a href="#">{line.content}</a></p>
+            }
+          })
+        }
       </div>
 
       <form className={styles.commentForm}>
