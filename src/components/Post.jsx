2b2a1928279168bc07e87122a6a8857f09c29b3d
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-02-14 20:21:34 -0300

    Entendendo a key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -53,9 +53,9 @@ export function Post({ author, publishedAt, content }) {
         {
           content.map(line => {
             if (line.type === 'paragraph') {
-              return <p>{line.content}</p>
+              return <p key={line.content}>{line.content}</p>
             } else {
-              return <p><a href="#">{line.content}</a></p>
+              return <p key={line.content}><a href="#">{line.content}</a></p>
             }
           })
         }
@@ -84,6 +84,7 @@ export function Post({ author, publishedAt, content }) {
           comments.map((comment) => {
             return (
               <Comment
+                key={comment}
                 content={comment}
               />
             )
